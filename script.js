396e22e15f967b2f64ae1b9bee46e9793258b3f3
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-06-23 08:59:52 +0100

    Add changes to js file and css file

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const btnmenu = document.getElementById('menu');
 const item = document.getElementById('item');
 
 const items = document.querySelectorAll('.item');
+const emailval = document.querySelector('.email');
+const submit = document.querySelector('.get-button1');
+const terror = document.getElementById('error');
+const Myname = document.querySelector('.name');
+const comments = document.getElementById('comments');
+
 items.forEach((it) => {
   it.addEventListener('click', () => {
     item.style = 'display: none;';
@@ -16,4 +22,18 @@ btnmenu.addEventListener('click', () => {
 
 btnclose.addEventListener('click', () => {
   item.style = 'display: none;';
+});
+
+// form
+const data = [];
+// 1
+submit.addEventListener('click', () => {
+  if (emailval.value === emailval.value.toLowerCase() && emailval.value !== '') {
+    data[0] = Myname.value;
+    data[1] = emailval.value;
+    data[2] = comments.value;
+    localStorage.setItem('local', JSON.stringify(data));
+  } else {
+    terror.textContent = 'error';
+  }
 });
